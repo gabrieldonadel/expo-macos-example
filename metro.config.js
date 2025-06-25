741b779d3e1be86d6f3b81f9c5a47411d2aeb985
Gabriel Donadel <donadeldev@gmail.com>
2025-06-25 17:39:58 -0300

    Use Expo CLI

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,33 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+// Learn more https://docs.expo.dev/guides/customizing-metro/
+const {getDefaultConfig} = require('expo/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (
+    platform === 'macos' &&
+    (moduleName === 'react-native' || moduleName.startsWith('react-native/'))
+  ) {
+    const newModuleName = moduleName.replace(
+      'react-native',
+      'react-native-macos',
+    );
+    return context.resolveRequest(context, newModuleName, platform);
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
+
+const originalGetModulesRunBeforeMainModule =
+  config.serializer.getModulesRunBeforeMainModule;
+config.serializer.getModulesRunBeforeMainModule = () => {
+  try {
+    return [
+      require.resolve('react-native/Libraries/Core/InitializeCore'),
+      require.resolve('react-native-macos/Libraries/Core/InitializeCore'),
+    ];
+  } catch {}
+  return originalGetModulesRunBeforeMainModule();
+};
+
+module.exports = config;
